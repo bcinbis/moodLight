a7710a49ac438107b3854e99c63c339ea4c0537f
Evan Hashemi <ehashemi@usc.edu>
2020-12-19 15:53:08 -0800

    added main.py and display.py to main branch

--- rpi/display.py
+++ rpi/display.py
@@ -1,45 +1,105 @@
+# Standard imports
+import os
 import sys
-# add paths to matrix python modules
+import time
+import string
+import fnmatch
+
+# Add paths to matrix python modules
 sys.path.append('rpi-rgb-led-matrix/bindings/python/rgbmatrix')
 sys.path.append('rpi-rgb-led-matrix/bindings/python/samples')
 
+# RPI and led matrix imports
 from PIL import Image
 from PIL import ImageDraw
+from rgbmatrix import graphics
 from samplebase import SampleBase
 from rgbmatrix import graphics, RGBMatrixOptions, RGBMatrix
-import time
 
-# Main function
-if __name__ == "__main__":
-    # Configuration for the matrix
-    options = RGBMatrixOptions()
-    options.rows = 32
-    options.cols = 32
-    options.chain_length = 1
-    options.parallel = 1
-    options.gpio_slowdown = 5
-    options.hardware_mapping = 'adafruit-hat'
 
-    matrix = RGBMatrix(options = options)
+class Display:
+    '''
+    Description: Basic superclass that contains the general startup procedures for the LED matrix
+    '''
+    def __init__(self):
+        
+        # Configuration for the matrix
+        options = RGBMatrixOptions()
+        options.rows = 32
+        options.cols = 32
+        options.chain_length = 1
+        options.parallel = 1
+        options.gpio_slowdown = 5
+        options.hardware_mapping = 'adafruit-hat' 
 
-    font = graphics.Font()
-    font.LoadFont("rpi-rgb-led-matrix/fonts/7x13.bdf")
+        # Enter in the hardware configurations
+        self.matrix = RGBMatrix(options = options)
 
-    COLORS = {
-        "red": [255, 0, 0],
-        "green": [0, 255, 0],
-        "blue": [0, 0, 255]
-    }
+        # Specifications for Font style of text
+        self.font = graphics.Font()
+        self.font.LoadFont("rpi-rgb-led-matrix/fonts/7x13.bdf")
 
-    image = Image.open("./img/green light.jpg")
-    # Make image fit our screen.
-    image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
-    matrix.SetImage(image.convert('RGB'))
+class codeDisplay(Display):
+    '''
+    This Display is used when the program is initially started. It is where the user can enter their 3-digit
+    code for the gathering they are attending.
+    '''
+    def __init__(self):
+        super().__init__()
 
-    try:
-        print("Press CTRL-C to stop.")
-        while True:
-            time.sleep(100)
-    except KeyboardInterrupt:
-        sys.exit(0)
+        # Variables used to display the possible code that the user has displayed
+        self.chars = ['_']
+        self.chars += list(string.ascii_lowercase)
+        self.str = ''
+        self.STR = ''
+        self.done = False
+
+    # Update led matrix with current selection of letters
+    def printStr(self):
+        self.matrix.Clear()
+        green = graphics.Color(0,255,0)
+        graphics.DrawText(self.matrix, self.font, 2, 10, green, self.str)
+
+    # Where index is updated through button presses in main,
+    # This allows for cycling through all of the alphabet
+    def sendIndex(self, index):
+        self.str =  self.STR + self.chars[index]
+        self.printStr()
+
+    # Signifies that the enter button has been pressed
+    def enter(self):
+        self.STR = self.str
+        if len(self.STR) == 3:
+            self.done = True
+        
+
+class imgDisplay(Display):
+    '''
+    This Display is used after a user joined the gathering and the client has downloaded all of the pictures
+    that the event has. Now the user can cycle through which picture they would like to display at any time
+    '''
+    def __init__(self):
+        super().__init__()
+
+        # Load all image names
+        self.image  = ''
+        self.index = 0
+        self.files = []
+        for f in os.listdir("testImg"):
+            if fnmatch.fnmatch(f, '*.jpg'):
+                self.files.append(f)
+        self.len = len(self.files)
+
+    # Print the current selected image to the LED matrix
+    def printImage(self):
+        self.matrix.Clear()
+        self.image = Image.open('./testImg/'+self.files[self.index])
+        self.image.thumbnail((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+        self.matrix.SetImage(self.image.convert('RGB'))
+
+    # Update which image is selected, occurs after button presses
+    def sendIndex(self, ind):
+        self.index = ind
+        self.printImage()
+
---
+++ rpi/main.py
@@ -0,0 +1,186 @@
+# Local imports from our files
+from display import codeDisplay, imgDisplay
+from client import Client
+import client
+
+# RPI pins import
+import RPi.GPIO as GPIO
+
+# Standard imports
+import time
+import fnmatch
+import os
+import sys
+
+# Variables for codeDisplay
+LENGTH = 27
+oldIndex = 0
+newIndex = 0
+enter = False
+# Button19 is for scrolling
+# Button18 is an enter key
+
+# Variables for imgDisplay
+nIndex = 0
+oIndex = 0
+imgNum = 0
+esc = False
+# Both buttons move through the different images
+# Hold either button for 4 seconds to leave the party
+
+# Flag to signify switch from codeD to imgD
+flag = True
+
+
+def button19_callback(channel):
+    global flag
+    '''
+    IF statement: Callback for button 19 during codeDisplay
+    Action: increases index (cycling back to 0 when max length is reached), index is sent (via main) 
+    to the led matrix where it controlls which ascii character (a to z) is currently selected
+    '''
+    if flag: 
+        time.sleep(.5)
+        print('button19')
+        global newIndex
+        global LENGTH
+
+        # Button Debounceing
+        while(GPIO.input(19) == GPIO.HIGH):
+            pass
+        
+        # Adjust index
+        newIndex += 1
+        if newIndex == LENGTH:
+            newIndex = 0
+
+    else:
+        '''
+        Else Satement: Callback during imgDisplay
+        Action 1: changes index, which is sent to imgDisplay (via main) which changes the image currently displayed on led matrix
+        Action 2: Hold down the button for more than 4 seconds and this will begin the ending sequence of the program
+        '''
+        global nIndex
+        global imgNum
+        global esc
+        time.sleep(.5)
+
+        # Track amount of time that button is held
+        start = time.perf_counter()
+        while(GPIO.input(19) == GPIO.HIGH):
+            pass
+        stop = time.perf_counter()
+
+        # Checks to see if button was held for more than 4 seconds
+        if (stop - start) > 4:
+            esc = True
+            return
+
+        # Adjust Index
+        nIndex += 1
+        if nIndex == imgNum:
+            nIndex = 0
+    
+
+def button18_callback(channel):
+    global flag
+    '''
+    IF Statement: Callback for button 18 during codeDisplay
+    Action: Works as an enter button. When it is pressed, it sends an enter code to the codeDisplay instance
+    (via main) to signify that the ascii selected is part of the code the user is trying to enter
+    '''
+    if flag:    
+        time.sleep(.5)
+        print('button18')
+        global enter
+        global newIndex
+
+        # Button Debouncing
+        while(GPIO.input(18) == GPIO.HIGH):
+            pass
+
+        # Enter was pressed
+        enter = True
+        newIndex = 0
+
+    
+    else:
+        '''
+        Else Satement: Callback during imgDisplay
+        Action 1: changes index, which is sent to imgDisplay (via main) which changes the image currently displayed on led matrix
+        Action 2: Hold down the button for more than 4 seconds and this will begin the ending sequence of the program
+        '''
+        global nIndex
+        global imgNum
+        global esc
+        time.sleep(.5)
+        
+        # Track amount of time that button is held
+        start = time.perf_counter()
+        while(GPIO.input(19) == GPIO.HIGH):
+            pass
+        stop = time.perf_counter()
+
+        # Checks to see if button was held for more than 4 seconds
+        if (stop - start) > 4:
+            esc = True
+            return
+        
+        # Adjust index
+        nIndex -= 1
+        if nIndex == -1:
+            nIndex = imgNum -1
+
+# Setting up interrupts for the buttons
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(19, GPIO.RISING, callback=button19_callback, bouncetime=400)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(18, GPIO.RISING, callback=button18_callback, bouncetime=400)
+
+
+def cleanup():
+    '''
+    Ensures that user only has images from their respective gathering
+    Action: This removes previous files from a past social gathering
+    '''
+    files = []
+    for f in os.listdir("testImg"):
+        if fnmatch.fnmatch(f, '*.jpg'):
+            files.append(f)
+    for f in files:
+            os.remove('./testImg/'+f)
+
+
+
+if __name__ == '__main__':
+    # Free up any storage 
+    cleanup()
+
+    # Enter your code for the gathering you are attending
+    disp = codeDisplay()
+    while not disp.done:
+        if enter:
+            disp.enter()
+            enter = False
+        if oldIndex != newIndex:
+            disp.sendIndex(newIndex)
+            oldIndex = newIndex
+    code = disp.STR # code is retrieved here
+    flag = False # adjust button callbacks to deal with seacond display
+    
+    # instert all client code
+    cli = Client(code)
+
+    # User can begin cycling through and choosing which image to display
+    disp = imgDisplay()
+    imgNum = disp.len
+    while not esc:
+        if oIndex != nIndex:
+            disp.sendIndex(nIndex)
+            oIndex = nIndex
+    
+    # ending/shutdown sequence
+    del disp
+    time.sleep(2)
+    sys.exit(0)
