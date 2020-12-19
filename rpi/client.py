7e5e3bfc9b424daffbf661916ea58f571373546d
lorandcheng <lfcheng@usc.edu>
2020-12-19 08:36:53 -0800

    created main

---
+++ rpi/client.py
@@ -0,0 +1,11 @@
+"""
+File for dealing with messaging interaction with server
+"""
+
+class Client:
+    def __init__(self, code):
+        self.partyCode = code
+
+    def getImages(self):
+        
+
