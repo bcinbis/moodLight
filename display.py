432c5ee80d9e4a1c290b87fd19f70783d795ad3c
lorandcheng <lfcheng@usc.edu>
2020-12-18 21:51:29 -0800

    testing again

--- display.py
+++ display.py
@@ -35,11 +35,7 @@ if __name__ == "__main__":
         "blue": [0, 0, 255]
     }
 
-    for x in range(0, matrix.width):
-        for y in range(0, matrix.height):
-            matrix.SetPixel(x, y, 0, 0, 0)
-
-    image = Image.open("./dont enter.jpg")
+    image = Image.open("./do_not_enter.jpg")
     # Make image fit our screen.
     image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
