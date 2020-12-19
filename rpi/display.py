a20c5a3e48eb40b1c1a66c95472c8443a316ded7
Evan Hashemi <ehashemi@usc.edu>
2020-12-19 13:11:33 -0800

    testing deletion of photos

--- rpi/display.py
+++ rpi/display.py
@@ -64,14 +64,24 @@ class imgDisplay(Display):
         self.image  = ''
         self.index = 0
         self.files = []
-        for f in os.listdir("img"):
+        for f in os.listdir("testImg"):
             if fnmatch.fnmatch(f, '*.jpg'):
                 self.files.append(f)
+        self.len = len(self.files)
 
     def printImage(self):
-        self.image = Image.open('./img/'+self.files[self.index])
+        self.image = Image.open('./testImg/'+self.files[self.index])
         self.image.thumbnail((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
         self.matrix.SetImage(self.image.convert('RGB'))
+
+    def sendIndex(self, ind):
+        self.index = ind
+        self.printImage()
+
+    def __del__(self):
+        for f in self.files:
+            os.remove('./testImg/'+f)
+
 
 
 
@@ -80,37 +90,35 @@ class imgDisplay(Display):
 
 # Main function
 if __name__ == "__main__":
-    # # Configuration for the matrix
-    # options = RGBMatrixOptions()
-    # options.rows = 32
-    # options.cols = 32
-    # options.chain_length = 1
-    # options.parallel = 1
-    # options.gpio_slowdown = 5
-    # options.hardware_mapping = 'adafruit-hat'
+    # Configuration for the matrix
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.cols = 32
+    options.chain_length = 1
+    options.parallel = 1
+    options.gpio_slowdown = 5
+    options.hardware_mapping = 'adafruit-hat'
 
-    # matrix = RGBMatrix(options = options)
+    matrix = RGBMatrix(options = options)
 
-    # font = graphics.Font()
-    # font.LoadFont("rpi-rgb-led-matrix/fonts/7x13.bdf")
+    font = graphics.Font()
+    font.LoadFont("rpi-rgb-led-matrix/fonts/7x13.bdf")
 
-    # COLORS = {
-    #     "red": [255, 0, 0],
-    #     "green": [0, 255, 0],
-    #     "blue": [0, 0, 255]
-    # }
+    COLORS = {
+        "red": [255, 0, 0],
+        "green": [0, 255, 0],
+        "blue": [0, 0, 255]
+    }
 
-    # image = Image.open("./img/green light.jpg")
-    # # Make image fit our screen.
-    # image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
+    image = Image.open("./img/green light.jpg")
+    # Make image fit our screen.
+    image.thumbnail((matrix.width, matrix.height), Image.ANTIALIAS)
 
-    # matrix.SetImage(image.convert('RGB'))
+    matrix.SetImage(image.convert('RGB'))
 
-    # try:
-    #     print("Press CTRL-C to stop.")
-    #     while True:
-    #         time.sleep(100)
-    # except KeyboardInterrupt:
-    #     sys.exit(0)
-    x = imgDisplay()
-    x.printImage()
+    try:
+        print("Press CTRL-C to stop.")
+        while True:
+            time.sleep(100)
+    except KeyboardInterrupt:
+        sys.exit(0)
--- rpi/main.py
+++ rpi/main.py
@@ -4,6 +4,7 @@ import client
 import RPi.GPIO as GPIO
 import time
 
+#variables for codeDisplay
 LENGTH = 27
 oldIndex = 0
 newIndex = 0
@@ -11,26 +12,84 @@ enter = False
 #button19 is for scrolling
 #button18 is for enter
 
+#variables for imgDisplay
+nIndex = 0
+oIndex = 0
+imgNum = 0
+esc = False
+#both buttons move through the different images
+#hold either button for 4 seconds to leave the party
+
+
+#flag to signify switch from codeD to imgD
+flag = True
+
+
+
 def button19_callback(channel):
-    time.sleep(.5)
-    print('button19')
-    global newIndex
-    global LENGTH
-    while(GPIO.input(19) == GPIO.HIGH):
-        pass
-    newIndex += 1
-    if newIndex == LENGTH:
-        newIndex = 0
+    global flag
+    if flag: 
+        time.sleep(.5)
+        print('button19')
+        global newIndex
+        global LENGTH
+
+        while(GPIO.input(19) == GPIO.HIGH):
+            pass
+        newIndex += 1
+
+        if newIndex == LENGTH:
+            newIndex = 0
+
+    else:
+        global nIndex
+        global imgNum
+        global esc
+
+        start = time.perf_counter()
+        while(GPIO.input(19) == GPIO.HIGH):
+            pass
+        stop = time.perf_counter()
+
+        if (stop - start) > 4:
+            esc = True
+            return
+
+        nIndex += 1
+        if nIndex == imgNum:
+            nIndex = 0
+    
 
 def button18_callback(channel):
-    time.sleep(.5)
-    print('button18')
-    global enter
-    global newIndex
-    while(GPIO.input(18) == GPIO.HIGH):
-        pass
-    enter = True
-    newIndex = 0
+    global flag
+    if flag:    
+        time.sleep(.5)
+        print('button18')
+        global enter
+        global newIndex
+
+        while(GPIO.input(18) == GPIO.HIGH):
+            pass
+
+        enter = True
+        newIndex = 0
+
+    else:
+        global nIndex
+        global imgNum
+
+        start = time.perf_counter()
+        while(GPIO.input(19) == GPIO.HIGH):
+            pass
+        stop = time.perf_counter()
+
+        if (stop - start) > 4:
+            esc = True
+            return
+        
+        nIndex -= 1
+        if nIndex == -1:
+            nIndex = imgNum -1
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -40,24 +99,28 @@ GPIO.add_event_detect(18, GPIO.RISING, callback=button18_callback, bouncetime=20
 
 
 
-def joinParty(code):
-    pass
-
-
-
-
 if __name__ == '__main__':
-    #joining a social gathering
-    disp = codeDisplay()
-    while not disp.done:
-        if enter:
-            disp.enter()
-            enter = False
-        if oldIndex != newIndex:
-            disp.sendIndex(newIndex)
-            oldIndex = newIndex
-    code = disp.STR
-    cli = Client(code)
-    #instert all client code
+    # #joining a social gathering
+    # disp = codeDisplay()
+    # while not disp.done:
+    #     if enter:
+    #         disp.enter()
+    #         enter = False
+    #     if oldIndex != newIndex:
+    #         disp.sendIndex(newIndex)
+    #         oldIndex = newIndex
+    # code = disp.STR
+    # flag = False
+    # cli = Client(code)
+    # #instert all client code
 
-    #changing the displayed image
+    # #changing the displayed image
+    # disp = imgDisplay()
+    # imgNum = disp.len
+    # while not esc:
+    #     if oIndex != nIndex:
+    #         disp.sendIndex(nIndex)
+    #         oIndex = nIndex
+    disp = imgDisplay()
+    time.sleep(2)
+    del disp
